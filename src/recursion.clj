5f84d53aeb57411251afd1e78fd52cae52a4c0f5
Christopher McCloud <mccloud.christopher@gmail.com>
2015-03-04 09:08:41 -0800

    adds seq=. passing 44 tests.

--- src/recursion.clj
+++ src/recursion.clj
@@ -58,7 +58,11 @@
         :else a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond (empty? a-seq) (empty? b-seq)
+        (and (= (first a-seq)
+                (first b-seq))) (seq= (rest a-seq)
+                                      (rest b-seq))
+                :else false))
 
 (defn my-map [f seq-1 seq-2]
   [:-])
