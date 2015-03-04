2b5aa53cde41063c549497b207cffe180ac1d0af
Christopher McCloud <mccloud.christopher@gmail.com>
2015-03-04 09:21:18 -0800

    adds my-repeat. passing 60 tests.

--- src/recursion.clj
+++ src/recursion.clj
@@ -83,7 +83,10 @@
          (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (<= how-many-times 0) '()
+      (cons (what-to-repeat
+             (my-repeat (dec how-many-times)
+                        what-to-repeat)))))
 
 (defn my-range [up-to]
   [:-])
