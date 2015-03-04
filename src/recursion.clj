c4b09cc1b620c23737c6ff21ad53fbb600105a72
Christopher McCloud <mccloud.christopher@gmail.com>
2015-03-03 21:22:15 -0800

    adds my-take-while. passing 35 tests.

--- src/recursion.clj
+++ src/recursion.clj
@@ -47,7 +47,10 @@
         :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond (empty? a-seq) a-seq
+        (pred? (first a-seq)) (cons (first a-seq)
+                                    (my-take-while pred? (rest a-seq)))
+        :else '()))
 
 (defn my-drop-while [pred? a-seq]
   [:-])
